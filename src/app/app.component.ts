6669c756ce857b340a4c4901393d62e01b23bc88
Adrian Albu <2412rock@gmail.com>
2024-10-08 13:27:22 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  inputText: string = '';
+  base64Result: string = '';
+
+  // Function to convert the string to Base64
+  convertToBase64(): void {
+    this.base64Result = btoa(unescape(encodeURIComponent(this.inputText)));
+  }
+
+  // Function to copy the result to the clipboard
+  copyToClipboard(): void {
+    navigator.clipboard.writeText(this.base64Result).then(() => {
+      //alert('Base64 string copied to clipboard!');
+    });
+  }
 }
